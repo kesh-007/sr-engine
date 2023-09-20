c89d6bc57dfb08db0a5386b0c84be1997de9c7ec
kesh-007 <keshavbaskar@yahoo.com>
2023-09-20 19:09:45 +0530

    profile page bug removed

--- apps/web/app/admin/profile/page.tsx
+++ apps/web/app/admin/profile/page.tsx
@@ -26,7 +26,7 @@ import Image from 'next/image'
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you are done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
